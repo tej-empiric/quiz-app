5b060f36baa619bd34c07801f023439592a67645
tej-empiric <tejas@empiricinfotech.com>
2024-04-23 18:37:35 +0530

    initial setup

---
+++ quiz/quiz_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("accounts/signup/", views.SignUpView.as_view(), name="signup"),
+    path("", views.home, name="home"),
+]
